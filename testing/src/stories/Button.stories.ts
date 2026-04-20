0fd8ff4a9e654e107a834c8cceb1504a482252a7
Roshan Bansie <roshan_b@outlook.com>
2026-04-20 21:59:43 +0200

    Fixed import issue.

--- testing/src/stories/Button.stories.ts
+++ testing/src/stories/Button.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import { fn } from 'storybook/test';
 
-import { Button } from "../../../lib"
+import { Button } from "../../../lib/components"
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
