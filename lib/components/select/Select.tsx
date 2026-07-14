b22a2bae4796a9a428d64e97aa4c85a554e1dc9d
Roshan Bansie <roshan_b@outlook.com>
2026-07-14 18:47:00 +0200

    Added documentation for the select component.

--- lib/components/select/Select.tsx
+++ lib/components/select/Select.tsx
@@ -6,14 +6,23 @@ import { FaCheck } from "react-icons/fa6"
 import { motion, AnimatePresence } from "motion/react"
 import "./select.css"
 
+// name: name of the select input which should be used to index the formData in
+// a formData change handler.
+// id: id of the select input.
+// values: array of selected option(s)
+// onSelect: callback function to pass selection to parent component.
+// options: array of options to choose from.
+// label: label above the select input.
+// multiple: allow multiple selections.
+// placeholder: placeholder for the select input.
+// disabled: disable input.
 type SelectProps = {
     name: string,
     id: string,
     values: (string | number)[],
-    onSelect: (name: string, value: (string | number)[]) => void,
+    onSelect: (name: string, value: string | (string | number)[]) => void,
     options: (string | number)[],
     label?: string,
-    required?: boolean,
     multiple?: boolean,
     placeholder?: string | number,
     disabled?: boolean
@@ -29,26 +38,36 @@ export default function Select({
     placeholder=options[0] ?? "Select an option",
     label,
     multiple=false,
-    required=true,
     disabled=false
 }: SelectProps) {
     const [open, setOpen] = React.useState<boolean>(false)
 
+    // Toggles the options list.
     function toggleOptions(): void {
         setOpen(prev => !prev)
     }
 
+    // Saves the currently selected option(s) and uses the callback function to pass
+    // it to the parent component.
     function handleSelect(selection: string | number): void {
+        // Concat option to selected options list if not present yet,
+        // otherwise remove it.
         if (multiple) {
             const newValue =
                     values.includes(selection) ? values.filter(prevOption => prevOption != selection)
                     : values.concat([selection])
             onSelect(name, newValue)
         } else {
+            // Select option or remove it if already present.
             const newValue = values[0] === selection ? [] : [selection]
             onSelect(name, newValue)
             toggleOptions()
         }
+    }
+
+    // Clip too long text.
+    function formatText(text: string) : string {
+        return text.length > 20 ? text.slice(0, 20) + "..." : text
     }
 
     return (
@@ -67,7 +86,9 @@ export default function Select({
                     return (
                         optionDisplay
                     )
-                }) : placeholder}
+                    }) 
+                    : typeof placeholder === "string" ? formatText(placeholder) : placeholder
+                }
                 <BiChevronDown 
                     className="open-options-icon" 
                     style={{transform: `rotate(${open ? 180 : 0}deg)`, transition: "0.25s"}}
@@ -77,7 +98,7 @@ export default function Select({
                 {open && options && options.length > 0 ? (
                     <motion.ul 
                         className="option-list"
-                        style={{top: label ? "60px" : "30px"}}
+                        style={{top: label ? "80px" : "50px"}}
                         initial={{scale: 0.95, opacity: 0}}
                         animate={{scale: 1, opacity: 1}}
                         exit={{scale: 0.95, opacity: 0}}
