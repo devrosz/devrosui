95419108f24baca5f54e8ab50bd302b50daa29c2
Roshan Bansie <roshan_b@outlook.com>
2026-08-08 15:14:27 +0200

    Button implementation and documentation.

--- testing/src/stories/Button.stories.ts
+++ testing/src/stories/Button.stories.ts
@@ -5,7 +5,7 @@ import { Button } from "@devrosui/react"
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
+  title: 'Button',
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
@@ -13,10 +13,6 @@ const meta = {
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#story-args
   args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
@@ -27,27 +23,32 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
+    style: "primary",
+    children: "Click me",
     label: 'Button',
   },
 };
 
 export const Secondary: Story = {
   args: {
+    style: "secondary",
+    children: "Click me",
     label: 'Button',
   },
 };
 
-export const Large: Story = {
+export const Tertiary: Story = {
   args: {
-    size: 'large',
+    style: "tertiary",
+    children: "Click me",
     label: 'Button',
   },
 };
 
-export const Small: Story = {
+export const Danger: Story = {
   args: {
-    size: 'small',
+    style: "danger",
+    children: "Click me",
     label: 'Button',
   },
 };
