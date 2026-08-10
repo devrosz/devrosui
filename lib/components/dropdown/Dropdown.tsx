d1910f838ca967f2e75ac74eeb91cd4562f347b1
Roshan Bansie <roshan_b@outlook.com>
2026-08-10 15:19:47 +0200

    Finalized styling and added comments.

--- lib/components/dropdown/Dropdown.tsx
+++ lib/components/dropdown/Dropdown.tsx
@@ -1,9 +1,21 @@
 "use client"
 
-import React from "react"
+import React, { JSX } from "react"
 import { createContext, useContext } from "react"
+import { AnimatePresence, motion } from "motion/react"
 import "./dropdown.css"
 
+// onClick: callback function that gets invoked when the option is clicked.
+// isDangerous: hints that this option is dangerous.
+// children: can be either a string or a custom component.
+type OptionProps = {
+    onClick?: () => void,
+    isDangerous?: boolean,
+    children: string | JSX.Element
+}
+
+// open: openstate of the option list.
+// toggle: callback function to toggle the open state.
 type DropdownContextType = {
     open: boolean,
     toggle: () => void
@@ -11,7 +23,9 @@ type DropdownContextType = {
 
 const DropdownContext = createContext<DropdownContextType | null>(null)
 
-export function Dropdown({children}) {
+// Wrapper container of the dropdown.
+// Passes the DropdownContext to the children.
+export function Dropdown({children}: {children: React.ReactNode}) {
     const [open, setOpen] = React.useState<boolean>(false)
 
     function toggle() {
@@ -27,7 +41,8 @@ export function Dropdown({children}) {
     )
 }
 
-export function Header({children}) {
+// Clickable button that allows the option list to popup.
+export function Header({children}: {children: string | JSX.Element}) {
 
     const dropDownContext = useContext(DropdownContext)
 
@@ -47,7 +62,9 @@ export function Header({children}) {
     )
 }
 
-export function List({children}) {
+// Option list that renders when the open state is set to 'true'.
+// Contains
+export function List({children}: {children: React.ReactNode}) {
     const dropDownContext = useContext(DropdownContext)
 
     if (!dropDownContext) {
@@ -57,21 +74,49 @@ export function List({children}) {
 
     const { open, toggle } = dropDownContext
 
-    return open ? (
-        <ul className="dropdown-list">
-            {children}
-        </ul>
-    ) : null
+    return (
+        <AnimatePresence>
+            {open ? (
+                <motion.ul 
+                    className="dropdown-list"
+                    initial={{scale: 0.9, opacity: 0}}
+                    animate={{scale: 1, opacity: 1}}
+                    exit={{scale: 0.9, opacity: 0}}
+                    transition={{duration: 0.1}}
+                >
+                    {children}
+                </motion.ul>
+
+            ): null}
+        </AnimatePresence>
+    )
 }
 
-export function Option({onClick, children}) {
+// Option that fires the onClick callback if applicable and closes the list popup.
+export function Option({onClick, isDangerous=false, children}: OptionProps) {
+    const dropDownContext = useContext(DropdownContext)
+
+    if (!dropDownContext) {
+        console.error("InputOTP.Header: context is undefined")
+        return
+    }
+
+    const { open, toggle } = dropDownContext
+
+
+    function handleClick() {
+        toggle()
+        if (onClick) {onClick()}
+    }
+
     return (
-        <li className="dropdown-option">
-            <button onClick={onClick}>{children}</button>
+        <li className={"dropdown-option " + (isDangerous ? "danger" : "")}>
+            <button onClick={handleClick}>{children}</button>
         </li>
     )
 }
 
+// Define subcomponents.
 Dropdown.Header = Header
 Dropdown.List = List
 Dropdown.Option = Option
--- testing/src/stories/Dropdown.stories.tsx
+++ testing/src/stories/Dropdown.stories.tsx
@@ -1,5 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/react-vite"
 import { Dropdown } from "@devrosui/react"
+import { AiOutlineEdit } from "react-icons/ai"
+import { FiSave } from "react-icons/fi";
+import { IoCopyOutline } from "react-icons/io5"
+import { RiDeleteBinLine } from "react-icons/ri"
 
 const meta = {
     title: "Dropdown",
@@ -25,12 +29,70 @@ export const DropdownDev: Story = {
                     Edit file
                 </Dropdown.Option>
                 <Dropdown.Option onClick={() => console.log('rename')}>
-                    Rename file
+                   Save file
                 </Dropdown.Option>
                 <Dropdown.Option onClick={() => console.log('copy')}>
                     Copy file
                 </Dropdown.Option>
                 <Dropdown.Option onClick={() => console.log('delete')}>
+                    Delete file
+                </Dropdown.Option>
+            </Dropdown.List>
+        </Dropdown>
+        )
+    }
+}
+
+export const DropdownDanger: Story = {
+    args: {},
+    render: () => {
+        return (
+        <Dropdown>
+            <Dropdown.Header>
+                Actions
+            </Dropdown.Header>
+            <Dropdown.List>
+                <Dropdown.Option onClick={() => console.log('edit')}>
+                    Edit file
+                </Dropdown.Option>
+                <Dropdown.Option onClick={() => console.log('rename')}>
+                   Save file
+                </Dropdown.Option>
+                <Dropdown.Option onClick={() => console.log('copy')}>
+                    Copy file
+                </Dropdown.Option>
+                <Dropdown.Option onClick={() => console.log('delete')} isDangerous={true}>
+                    Delete file
+                </Dropdown.Option>
+            </Dropdown.List>
+        </Dropdown>
+        )
+    }
+}
+
+export const DropdownIcons: Story = {
+    args: {},
+    render: () => {
+        return (
+        <Dropdown>
+            <Dropdown.Header>
+                Actions
+            </Dropdown.Header>
+            <Dropdown.List>
+                <Dropdown.Option onClick={() => console.log('edit')}>
+                    <AiOutlineEdit />
+                    Edit file
+                </Dropdown.Option>
+                <Dropdown.Option onClick={() => console.log('rename')}>
+                    <FiSave />
+                   Save file
+                </Dropdown.Option>
+                <Dropdown.Option onClick={() => console.log('copy')}>
+                    <IoCopyOutline />
+                    Copy file
+                </Dropdown.Option>
+                <Dropdown.Option onClick={() => console.log('delete')} isDangerous={true}>
+                    <RiDeleteBinLine />
                     Delete file
                 </Dropdown.Option>
             </Dropdown.List>
