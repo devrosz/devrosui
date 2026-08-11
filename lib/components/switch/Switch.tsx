b860c7be93e9f4e536ef65f1da0a94d524b78c8e
Roshan Bansie <roshan_b@outlook.com>
2026-08-11 14:39:39 +0200

    Changed architecture and added new props.

--- lib/components/switch/Switch.tsx
+++ lib/components/switch/Switch.tsx
@@ -1,27 +1,111 @@
 "use client"
 
 import React from "react"
+import { createContext, useContext, JSX } from "react"
 import "./switch.css"
 
-export default function Switch() {
-    const [active, setActive] = React.useState<boolean>(false)
+type SwitchProps = {
+    isActive?: boolean,
+    onActive?: () => Promise<void> | void,
+    onDeactive?: () => Promise<void> | void,
+    disabled: boolean
+    children: React.ReactNode
+}
+
+type SwitchContextObj = {
+    active: boolean,
+    handleClick: () => void,
+    disabled: boolean
+}
+
+type TrackProps = {
+    children: Thumb
+}
+
+type ThumbProps = {
+    children?: JSX.Element
+}
+
+const SwitchContext = createContext<SwitchContextObj | null>(null)
+
+function Switch({isActive=false, onActive, onDeactive, disabled=false, children}: SwitchProps) {
+    const [active, setActive] = React.useState<boolean>(isActive)
 
     function handleClick() {
-        setActive(prev => !prev)
+        const newState: boolean = !active
+        setActive(newState)
+
+        // A separate variable is used to hold the new value so that
+        // the activation functions can be fired immediately and not 1 render too late.
+        if (onActive && newState) {onActive()}
+        if (onDeactive && !newState) {onDeactive()}
     }
 
     return (
-        <div className="switch-container">
-            <button 
-                className={"switch-track " + (active ? "active" : "")}
-                onClick={handleClick}
-            >
-                <div
-                    className="switch-thumb"
-                    style={{transform: `translateX(${active ? "34px" : 0})`}}
-                >
-                </div>
-            </button>
+        <SwitchContext.Provider value={{active, handleClick, disabled}}>
+            <div className="switch-container">
+               {children}
+            </div>
+        </SwitchContext.Provider>
+    )
+}
+
+function Track({children}: TrackProps) {
+    const context = useContext(SwitchContext)
+
+    if (!context) {
+        console.error("Switch.Track: context is undefined.")
+        return
+    }
+
+    const { active, handleClick } = context
+
+    return (
+         <button 
+            className={"switch-track " + (active ? "active" : "")}
+            onClick={handleClick}
+        >
+            {children}
+        </button>
+    )
+}
+
+function Thumb({children}: ThumbProps) {
+    const context = useContext(SwitchContext)
+
+    if (!context) {
+        console.error("Switch.Track: context is undefined.")
+        return
+    }
+
+    const { active, handleClick } = context
+
+    return (
+        <div
+            className="switch-thumb"
+            style={{transform: `translateX(${active ? "26px" : 0})`}}
+        >
+            {children && children}
         </div>
     )
 }
+
+function Meta({children}: {children: JSX.Element | JSX.Element[]}) {
+    return <div className="switch-meta">{children}</div>
+}
+
+function Label({children}: {children: string}) {
+    return <h5 className="switch-label">{children}</h5>
+}
+
+function Description({children}: {children: string}) {
+    return <p className="switch-description">{children}</p>
+}
+
+Switch.Track = Track
+Switch.Thumb = Thumb
+Switch.Meta = Meta
+Switch.Label = Label
+Switch.Description = Description
+
+export default Switch
