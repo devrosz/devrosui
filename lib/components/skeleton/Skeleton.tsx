fa2d22b0af70973a514e70e1eeb199d09415b60d
Roshan Bansie <roshan_b@outlook.com>
2026-08-24 13:28:10 +0200

    Added comments.

--- lib/components/skeleton/Skeleton.tsx
+++ lib/components/skeleton/Skeleton.tsx
@@ -1,11 +1,16 @@
 import "./skeleton.css"
 
+// height: height of the skeleton.
+// width: width of the skeleton.
+// borderRadius: border-radius of the skeleton.
+// Type 'string' is used to allow the user to specify the unit.
 type SkeletonProps = {
     height?: string,
     width?: string,
     borderRadius?: string
 }
 
+// Placeholder for page elements with animating gradient background
 export default function Skeleton({height="100px", width="200px", borderRadius="16px"}: SkeletonProps) {
     return (
         <div 
