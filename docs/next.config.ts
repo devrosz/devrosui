e2f25597c2d5a4c1140d241b71459737fa5cdb84
Roshan Bansie <roshan_b@outlook.com>
2026-08-31 21:56:11 +0200

    Removed one unused import.

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from "next"
 import createMDX from "@next/mdx"
-import remarkGfm from "remark-gfm"
 
 const nextConfig: NextConfig = {
   pageExtensions: ['tsx', 'ts', 'mdx'],
