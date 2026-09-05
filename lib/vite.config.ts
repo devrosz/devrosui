64029f6d7dcad156a69fc2cb4bac7e7344519b0b
Roshan Bansie <roshan_b@outlook.com>
2026-09-05 13:29:17 +0200

    Changed config files to prepare correct npm pack.

--- lib/vite.config.ts
+++ lib/vite.config.ts
@@ -1,18 +1,39 @@
-import { defineConfig } from "vite"
+import { defineConfig, esmExternalRequirePlugin } from "vite"
 import react from "@vitejs/plugin-react"
+import dts from "vite-plugin-dts"
 
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        lib: {
-            entry: ['./src/index.ts'],
-            formats: ['es', 'cjs'],
-            name: '@devrosui/react',
-            cssFileName: 'style'
-        },
-        // Don't ship React with the library and let the user use his own React.
-        rolldownOptions: {
-            external: ['react', 'react-dom']
-        }
-    }
+  plugins: [
+    react(),
+    dts(),
+    esmExternalRequirePlugin({
+      external: [
+        /^react($|\/)/,
+        /^react-dom($|\/)/,
+        /^motion($|\/)/,
+        /^react-icons($|\/)/,
+        /^@fontsource\/geist($|\/)/,
+      ],
+    }),
+  ],
+
+  build: {
+    lib: {
+      entry: "./src/index.ts",
+      fileName: "index",
+      formats: ["es", "cjs"],
+      name: "@devrosui/react",
+      cssFileName: "style",
+    },
+
+    rolldownOptions: {
+      external: [
+        /^react($|\/)/,
+        /^react-dom($|\/)/,
+        /^motion($|\/)/,
+        /^react-icons($|\/)/,
+        /^@fontsource\/geist($|\/)/,
+      ],
+    },
+  },
 })
