cc628f507a4687dd320aafec0e327a58a77545a5
Roshan Bansie <roshan_b@outlook.com>
2026-09-21 17:17:59 +0200

    Fixed navbar scroll issue on mobile.

--- docs/src/components/layout/navbar/MobileNav.tsx
+++ docs/src/components/layout/navbar/MobileNav.tsx
@@ -36,66 +36,69 @@ export default function MobileNav({open, toggle}: MobileNavProps) {
     }
 
     return (
-        <motion.ul
-            className="toggle-menu"
+        <motion.div 
+            className="toggle-menu-container"
             initial={{height: "0"}}
             animate={{height: open ? "100vh" : "0"}}
             transition={{duration: 0.5, ease: "easeInOut"}}
-            style={{overflowX: "hidden", overflowY: "scroll"}}
+            style={{overflowX: "hidden"}}
         >
-            {/* User actions */}
-            <li key="user-actions">
-                <h6>User actions</h6>
-                <div className="user-actions-container">
-                    <ThemeToggle toggleFunction={toggleTheme} />
-                    <Link href="https://github.com/devrosz/devrosui" className="github-link-btn">
-                        <IoLogoGithub style={{height: 25, width: 25}} />
-                    </Link>
-                </div>
-            </li>
 
-
-            {/* Site pages */}
-            <li key="mobile-nav-pages">
-                <h6>Pages</h6>
-            </li>
-            {pages.map(page => {
-                const { title, path } = page
-                return (
-                    <li 
-                        onClick={() => toggle()}
-                        key={path}
-                        className={checkIsActive(path) ? "active" : ""}
-                    >
-                        <Link href={path}>
-                            {title}
+            <ul className="toggle-menu">
+                {/* User actions */}
+                <li key="user-actions">
+                    <h6>User actions</h6>
+                    <div className="user-actions-container">
+                        <ThemeToggle toggleFunction={toggleTheme} />
+                        <Link href="https://github.com/devrosz/devrosui" className="github-link-btn">
+                            <IoLogoGithub style={{height: 25, width: 25}} />
                         </Link>
-                    </li>
-                )
-            })}
-            
-            {/* Documentation pages */}
-            {Object.entries(docsPages).map(([section, pages]) => (
-                <React.Fragment key={section}>
-                    <li key={section}>
-                        <h6>{section}</h6>
-                    </li>
-                        {pages.map((page) => {
-                            const { title, path } = page
-                            return (
-                                <li 
-                                    onClick={() => toggle()}
-                                    key={path}
-                                    className={checkIsActive(path) ? "active" : ""}
-                                >
-                                    <Link href={path}>
-                                        {title}
-                                    </Link>
-                                </li>
-                            )
-                        })}
-                </React.Fragment>
-            ))}
-        </motion.ul>
+                    </div>
+                </li>
+
+
+                {/* Site pages */}
+                <li key="mobile-nav-pages">
+                    <h6>Pages</h6>
+                </li>
+                {pages.map(page => {
+                    const { title, path } = page
+                    return (
+                        <li 
+                            onClick={() => toggle()}
+                            key={path}
+                            className={checkIsActive(path) ? "active" : ""}
+                        >
+                            <Link href={path}>
+                                {title}
+                            </Link>
+                        </li>
+                    )
+                })}
+                
+                {/* Documentation pages */}
+                {Object.entries(docsPages).map(([section, pages]) => (
+                    <React.Fragment key={section}>
+                        <li key={section}>
+                            <h6>{section}</h6>
+                        </li>
+                            {pages.map((page) => {
+                                const { title, path } = page
+                                return (
+                                    <li 
+                                        onClick={() => toggle()}
+                                        key={path}
+                                        className={checkIsActive(path) ? "active" : ""}
+                                    >
+                                        <Link href={path}>
+                                            {title}
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                    </React.Fragment>
+                ))}
+            </ul>
+        </motion.div>
     )
 }
